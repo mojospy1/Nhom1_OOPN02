2476ca20a59c4c709c2ea7d155988ece9452a9d0
mojospy1 <zackdeaths@gmail.com>
2025-06-14 14:29:07 +0700

    fix_merge

--- src/main/java/com/mycompany/quanlygym/action/CheckLogin.java
+++ src/main/java/com/mycompany/quanlygym/action/CheckLogin.java
@@ -2,33 +2,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-<<<<<<< HEAD:src/main/java/com/mycompany/quanlygym/action/CheckLogin.java
+//<<<<<<< HEAD:src/main/java/com/mycompany/quanlygym/action/CheckLogin.java
 package com.mycompany.quanlygym.action;
-
-import com.mycompany.quanlygym.entity.User;
-=======
-package com.mycompany.quanlydoituongdacbiet.action;
 import java.util.List;
 import java.util.ArrayList;
-import com.mycompany.quanlydoituongdacbiet.entity.User;
->>>>>>> ee5768bb1962bb638945a58b6fc3284afdade289:src/main/java/com/mycompany/quanlydoituongdacbiet/action/CheckLogin.java
+import com.mycompany.quanlygym.entity.User;
+//=======
+//package com.mycompany.quanlydoituongdacbiet.action;
+//import java.util.List;
+//import java.util.ArrayList;
+//import com.mycompany.quanlydoituongdacbiet.entity.User;
+//>>>>>>> ee5768bb1962bb638945a58b6fc3284afdade289:src/main/java/com/mycompany/quanlydoituongdacbiet/action/CheckLogin.java
 
 /**
  *
  * @author PC
  */
 public class CheckLogin {
-<<<<<<< HEAD
-    public boolean checkUser(User user) {
-        if (user != null) {
-            if ("admin".equals(user.getUserName()) 
-                    && "admin".equals(user.getPassword())) {
-                return true;
-            }
-        }
-        return false;
-//            return true ;
-=======
+//<<<<<<< HEAD
+//    public boolean checkUser(User user) {
+//        if (user != null) {
+//            if ("admin".equals(user.getUserName()) 
+//                    && "admin".equals(user.getPassword())) {
+//                return true;
+//            }
+//        }
+//        return false;
+////            return true ;
+//=======
    private static List<User> users = new ArrayList<>();
    
    static{
@@ -43,7 +44,7 @@ public class CheckLogin {
            }
        }
           return false ;
->>>>>>> a5551f4 (login)
+//>>>>>>> a5551f4 (login)
     }
 }
 //cc
